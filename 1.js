1a4fc61e423e94225ee5d3d1d0f4ed6591b8b9cb
wuqiong <wuqiong@DuoLunTechnology.com>
2016-12-19 17:00:51 +0800

    add my test

--- 1.js
+++ 1.js
@@ -4,3 +4,25 @@ var windowWidth=$(window).width(),
 var shotTop=150;
 var headerHeight=800;
 $shot.height(headerHeight-shotTop);
+
+// œÓƒøΩÈ…‹¬÷≤•
+var $ct=$(".project-list");
+var $list=$(".project-introduction");
+$list.on('click',function(){ 
+   var $cur=$(this);
+  console.log(idx);
+   var idx=$cur.index();
+  $ct.children().attr('class','project-center');
+  if(idx===1){
+    $cur.prev().attr('class','project-left');
+    $cur.next().attr('class','project-right');
+  }
+  if (idx===0){
+    $list.eq(2).attr('class','project-left');
+    $cur.next().attr('class','project-right');
+  }
+  if (idx===2){
+      $cur.prev().attr('class','project-left');
+      $list.eq(0).attr('class','project-right');
+  }
+});
