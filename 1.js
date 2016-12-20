f33e071c2acf76bb0d14490ec5e1796fe10dfbff
wuqiong <wuqiong@DuoLunTechnology.com>
2016-12-20 11:08:54 +0800

    add my test

--- 1.js
+++ 1.js
@@ -4,9 +4,88 @@ var windowWidth=$(window).width(),
 var shotTop=150;
 var headerHeight=800;
 $shot.height(headerHeight-shotTop);
+var $teacherList=$('.teacher-list'),$teacherLi=$('.teacher-list li'),$arrowBefore=$('.arrow-before'),$arrowAfter=$('.arrow-after'),$dot=$('.dot');
+var windowWidth=$(window).width();
+var liWidth=windowWidth*0.8;
+$teacherLi.width(liWidth);
+// ÂÖ²¥
+function foo2(){
+var lisWidth=$teacherLi.width();
+var $lis=$teacherList.children(), liCount=$lis.length;
+$teacherList.append($teacherLi.first().clone());
+$teacherList.prepend($teacherLi.last().clone());
 
+var liRealcount=$teacherList.children().length;
+console.log(liRealcount);
+
+$teacherList.css({
+  left: -lisWidth,
+  top: 0
+});
+
+var curIdx=0;
+$dot.children().on('click',function(){
+  var $cur=$(this),
+      idx=$cur.index();
+  if(idx<curIdx){
+    playPre(curIdx-idx);
+  }else if(idx>curIdx){
+    playNext(idx-curIdx);
+  }
+});
+$arrowAfter.on('click',function(){
+  playNext();
+});
+$arrowBefore.on('click',function(){
+  playPre();
+});
+var isAnimate=false;
+function playNext(idx){
+ idx=idx||1;
+  if(!isAnimate){
+    isAnimate=true;
+    $teacherList.animate({left: '-='+lisWidth*idx},200,function(){
+      curIdx=(curIdx+idx)%liCount;
+      if(curIdx===0){
+        $teacherList.css({
+          left: -lisWidth
+        });
+      }
+      isAnimate=false;
+      setButtle();
+    });
+    
+  }
+}
+function playPre(idx){
+ idx=idx||1;
+  if(!isAnimate){
+    isAnimate=true;
+    $teacherList.animate({left: '+='+lisWidth*idx},200,function(){
+      curIdx=(curIdx+liCount-idx)%liCount;
+      if(curIdx===(liCount-1)){
+        $teacherList.css({
+          left: -lisWidth*liCount
+        });
+      }
+      isAnimate=false;
+      setButtle();
+    });
+    
+  }
+}
+function setButtle(){      $dot.children().removeClass('active').eq(curIdx).addClass('active');
+}
+function autoPlay(){
+        clock=setInterval(function(){
+            playNext();
+        },3000);
+    }
+// autoPlay();
+}
 // ÏîÄ¿½éÉÜÂÖ²¥
-var $ct=$(".project-list");
+function foo(){
+	var $ct=$(".project-list");
 var $list=$(".project-introduction");
 $list.on('click',function(){ 
    var $cur=$(this);
@@ -26,3 +105,6 @@ $list.on('click',function(){
       $list.eq(0).attr('class','project-right');
   }
 });
+}
+foo();
+foo2();
